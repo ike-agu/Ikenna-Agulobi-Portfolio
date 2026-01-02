e3bd3246df5c5bb952a2112976f37d0bfaf7759a
ike-agu <ikennaww@yahoo.fr>
2026-01-02 12:41:25 +0000

    Install react-router-dom and set up project structure

--- Ikenna-portfolio/src/main.jsx
+++ Ikenna-portfolio/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+// import ".styles/global.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
