631e6daec45292ad682f18bacd411fe5ddff8288
ike-agu <ikennaww@yahoo.fr>
2026-01-03 10:21:57 +0000

    feat: implement Projects page with dynamic grid rendering

--- Ikenna-portfolio/src/pages/Projects.jsx
+++ Ikenna-portfolio/src/pages/Projects.jsx
@@ -1,8 +1,19 @@
+import ProjectCard from "../components/ProjectCard";
+import {projects} from "../data/projects";
+
 export default function Projects() {
   return (
       <section className="container section">
-        <h1>Projects Page</h1>
-        <p className="mute">Ikenna Agulobi Projects</p>
+        <h1>Projects</h1>
+        <p className="mute">
+          Below are Some projects that I've built, focusing on clean UI, JavaScript fundamentals, APIs and real world delivery
+        </p>
+
+        <div className="grid">
+          {projects.map((project)=>(
+            <ProjectCard key={project.title} project={project}/>
+          ))}
+        </div>
       </section>
   );
 }
