527eac6cf31fcbbbf9a6ebe13f150ae52085539d
ike-agu <ikennaww@yahoo.fr>
2026-01-03 12:39:57 +0000

    feat: add Hero section with profile image and CTA button

--- Ikenna-portfolio/src/pages/Home.jsx
+++ Ikenna-portfolio/src/pages/Home.jsx
@@ -1,15 +1,36 @@
+import { Link } from "react-router-dom";
+import profileImg from "../assets/images/Ikenna_07.png"
+
 export default function Home() {
   return (
-    <>
-    <h1>Home page </h1>
-    <h1>Junior Frontend Developer</h1>
-    <p className="lead">
-      I build clean, responsive web apps using JavaScript and React.
-    </p>
-    <p className="muted">
-      React portfolio. Projects coming soon....
-    </p>
-    </>
+    <section className="container section">
+      <div className="hero">
+        <p className="kicker">Junior Frontend Developer</p>
+        <h1 className="hero_title">
+          I build clean, responsive web apps using JavaScript and React.
+        </h1>
+        <p className="lead">
+          I focus on clear UI, solid fundamentals, and real-world projects —
+          from API-driven apps to production websites.
+        </p>
 
+        <div className="hero_actions">
+          <Link className="btn" to="/projects">
+            View Projects
+          </Link>
+        </div>
+
+        <p className="muted">
+          Based in the UK, Open to junior frontend opportunities
+        </p>
+        <div className="hero__imageWrap">
+          <img
+            className="hero__image"
+            src={profileImg}
+            alt="Portrait placeholder"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
